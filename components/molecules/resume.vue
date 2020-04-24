204ec3b0cb41bdfbc35e606095da85ff107ab586
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-24 11:47:34 +0200

    [Update]resume.vue remove frameborder

--- components/molecules/resume.vue
+++ components/molecules/resume.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src="https://drive.google.com/file/d/1NcODE6iHm-q1_FmDnhHeCclQSAXqSpco/preview">
+  <iframe src="https://drive.google.com/file/d/1NcODE6iHm-q1_FmDnhHeCclQSAXqSpco/preview" frameborder="0">
     <span>{{ $t('error.errorPreview') }}</span>
   </iframe>
 </template>
